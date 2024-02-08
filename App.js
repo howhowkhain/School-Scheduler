a2e6d99be513c037d0dab1944fb1d77127dff09b
howhowkhain <howhowkhain@yahoo.co.uk>
2024-02-08 23:16:00 +0200

    Day Schedule Page

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import HomePage from "./app/components/HomePage";
+import DaySchedule from "./app/components/DaySchedule";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <HomePage />
+      <DaySchedule />
       <StatusBar style="auto" />
     </View>
   );
